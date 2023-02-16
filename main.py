6a4f44a121e118c240848e30e22b9414033f4a45
Logan Marchione <logan@loganmarchione.com>
2023-02-15 20:21:37 -0500

    Make the linter happpy

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ Base.metadata.create_all(engine)
 # Initialize the app
 app = FastAPI()
 
+
 # Helper function to get database session
 def get_session():
     session = SessionLocal()
@@ -19,9 +20,11 @@ def get_session():
     finally:
         session.close()
 
+
 @app.get("/")
 def root():
     return "todo"
+
 
 @app.post("/todo", response_model=schemas.ToDo, status_code=status.HTTP_201_CREATED)
 def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session)):
@@ -37,6 +40,7 @@ def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session
     # return the todo object
     return tododb
 
+
 @app.get("/todo/{id}", response_model=schemas.ToDo)
 def read_todo(id: int, session: Session = Depends(get_session)):
 
@@ -48,6 +52,7 @@ def read_todo(id: int, session: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
 
     return todo
+
 
 @app.put("/todo/{id}", response_model=schemas.ToDo)
 def update_todo(id: int, task: str, session: Session = Depends(get_session)):
@@ -66,6 +71,7 @@ def update_todo(id: int, task: str, session: Session = Depends(get_session)):
 
     return todo
 
+
 @app.delete("/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_todo(id: int, session: Session = Depends(get_session)):
 
@@ -81,6 +87,7 @@ def delete_todo(id: int, session: Session = Depends(get_session)):
 
     return None
 
+
 @app.get("/todo", response_model = List[schemas.ToDo])
 def read_todo_list(session: Session = Depends(get_session)):
 
@@ -88,6 +95,7 @@ def read_todo_list(session: Session = Depends(get_session)):
     todo_list = session.query(models.ToDo).all()
 
     return todo_list
+
 
 @app.get("/health")
 def root():
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+
 class ToDo(Base):
     __tablename__ = 'todo'
-    id   = Column(Integer, primary_key=True)
+    id =   Column(Integer, primary_key=True)
     task = Column(String)
